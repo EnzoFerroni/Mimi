5fe02f12a33e9f7f7827dc9c908f447b6d7c95f1
LuccaPV <146880715+LuccaPV@users.noreply.github.com>
2025-05-30 17:13:27 -0300

    Add files via upload
    Um monte de arquivos

---
+++ POC_Calendar/POC_Calendar/POC_CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  POC_CalendarApp.swift
+//  POC_Calendar
+//
+//  Created by Aluno Mack on 28/05/25.
+//
+
+import SwiftUI
+
+@main
+struct POC_CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
